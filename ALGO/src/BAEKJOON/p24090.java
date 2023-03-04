397c52544a553934b9c70698aa9943c5b54c3122
qkdkran123 <qkdkran123@gmail.com>
2023-03-05 00:00:49 +0900

    0304studying

---
+++ ALGO/src/BAEKJOON/p24090.java
@@ -0,0 +1,18 @@
+package BAEKJOON;
+
+import java.io.*;
+
+public class p24090 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
+
+class QuickSort1 {
+    
+}
