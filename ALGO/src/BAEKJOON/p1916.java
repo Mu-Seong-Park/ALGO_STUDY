2eb709d3ac5e9865c02cf054ffcac539b9c3ee8c
qkdkran123 <qkdkran123@gmail.com>
2023-03-27 18:48:19 +0900

    temp

---
+++ ALGO/src/BAEKJOON/p1916.java
@@ -0,0 +1,16 @@
+package BAEKJOON;
+
+import java.io.*;
+
+public class p1916 {
+    //문제풀이
+    //1. 전형적인 다익스트라문제
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
